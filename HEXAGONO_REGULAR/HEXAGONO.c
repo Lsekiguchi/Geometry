6672c21240f48465a951f7e4072216b441a87b99
Lsekiguchi <108840988+Lsekiguchi@users.noreply.github.com>
2024-12-03 02:19:54 -0300

    Término do hexágono regular
    Término do código do hexágono regular. Em breve, começarei o código do octógono regular.

--- HEXAGONO_REGULAR/HEXAGONO.c
+++ HEXAGONO_REGULAR/HEXAGONO.c
@@ -3,9 +3,20 @@
 
 int main() {
     float lado, area, ccircunscrita, cinscrita, diagonal1, diagonal2;
+    const float r3 = 1.732;
     printf("Digite o valor do lado do hexagono regular: ");
     scanf("%f", &lado);
-
+    printf("\n");
+    ccircunscrita = lado;
+    printf("O raio da circunferencia circunscrita ao hexagono e: %f\n", ccircunscrita);
+    cinscrita = lado*r3/2;
+    printf("O raio da circunferencia inscrita ao hexagono e: %f\n", cinscrita);
+    diagonal1 = lado*r3;
+    diagonal2 = 2*lado;
+    printf("O tamanho da diagonal menor e: %f\n", diagonal1);
+    printf("O tamanho da diagonal maior e: %f\n", diagonal2);
+    area = 3*lado*lado*r3/2;
+    printf("A area do hexagono e: %f\n\n", area);
     system("pause");
     return 0;
 }
