1bcdb50df99754d6bcf493e55ada654688497340
Lsekiguchi <108840988+Lsekiguchi@users.noreply.github.com>
2024-12-16 20:08:56 -0300

    Adição do dodecaedro regular
    O arquivo do dodecaedro regular ainda não está completo

---
+++ DODECAEDRO REGULAR/DODECAEDRO_REGULAR.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Digite o valor do lado do dodecaedro regular: ");
+    float lado12, area12, ccircunscrita12, cinscrita12, diagonal121, diagonal122, diagonal123, diagonal124, diagonal125;
+    scanf("%f", &lado12);
+    ccircunscrita12 = lado12*sqrt(2)/(sqrt(3) - 1);
+    cinscrita12 = sqrt(4*ccircunscrita12*ccircunscrita12 - lado12*lado12)/2;
+    diagonal121 = lado12*(sqrt(6) + sqrt(2))/2;
+
+    diagonal125 = 2*ccircunscrita12;
+    area12 = 3*ccircunscrita12*ccircunscrita12;
+    printf("\nO raio da circunferencia circunscrita e: %f\n", ccircunscrita12);
+    printf("O raio da circunferencia inscrita e: %f\n", cinscrita12);
+    printf("A menor diagonal mede: %f\n", diagonal121);
+
+    printf("A maior diagonal mede: %f\n", diagonal125);
+    printf("A area do dodecaedro e: %f\n\n", area12);
+
+    system("pause");
+    return 0;
+}
