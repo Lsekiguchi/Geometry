e55db6cfff87c866733994bbb296a68aa563e460
Lsekiguchi <108840988+Lsekiguchi@users.noreply.github.com>
2025-01-03 12:56:10 -0300

    Organização da pasta de geometria

---
+++ programa_generalizado/programa_generalizado.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    printf("Digite o número de lados do polígono regular: ");
+    unsigned int num, diagnum, aux; //num -> n° de lados; diagnum -> n° de diagonais
+    float lado, ang, ccircunscrita, cinscrita, arco; //lado -> tamanho dos lados; ang -> ângulo em cada vértice
+    const float pi = 3.14159265;
+    scanf("%i", &num);
+    printf("Digite o tamanho dos lados do polígono regular: ");
+    scanf("%f", &lado);
+    ang = pi*(num - 2)/num;
+    arco = 2*pi/num;
+    ccircunscrita = lado/(2*sin(arco/2));
+    printf("\nO raio da circunferência circunscrita ao polígono regular é: %f\n", ccircunscrita);
+    cinscrita = ccircunscrita*cos(arco/2);
+    printf("O raio da circunferência inscrita ao polígono regular é: %f\n", cinscrita);
+    diagnum = (num - 2)/2;
+    float diagonais[diagnum];
+    if (num == 3) {
+        printf("Não há diagonais\n\n");
+    } else {
+    diagonais[0] = lado*sqrt(2 - 2*cos(ang));
+    aux = 1;
+    while (aux <= diagnum - 1) {
+            diagonais[aux] = diagonais[aux - 1]*cos(arco/2) + sqrt(lado*lado - diagonais[aux - 1]*diagonais[aux - 1]*sin(arco/2)*sin(arco/2));
+            aux++;
+    }
+    aux = 1;
+    printf("As diagonais do polígono são: %f", diagonais[0]);
+    while (aux <= diagnum - 1) {
+        printf(", %f", diagonais[aux]);
+        aux++;
+    }
+    printf("\n\n");
+    }
+
+
+
+
+
+    system("pause");
+    return 0;
+}
